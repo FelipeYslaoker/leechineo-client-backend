4d1b2a4ad67033ea7d3a7e7a2394659d26eebbed
FelipeYslaoker <felipeyslaok@gmail.com>
2022-11-26 13:45:33 -0300

    :sparkles: Added: Delete pending pix when cart are updated

--- src/app/models/CartItem.js
+++ src/app/models/CartItem.js
@@ -1,4 +1,5 @@
 const mongoose = require('../config/database')
+const PendingPix = require('./PendingPix')
 
 const CartItemSchema = mongoose.Schema({
     user: {
@@ -25,6 +26,14 @@ const CartItemSchema = mongoose.Schema({
         type: Number,
         required: true
     }
+})
+
+CartItemSchema.pre('save', async function (next) {
+    const pendingPix = await PendingPix.find({user: this.user})
+    for (const pix of pendingPix) {
+        await PendingPix.findByIdAndDelete(pix.id)
+    }
+    next()
 })
 
 const CartItem = mongoose.model('cartitems', CartItemSchema)
